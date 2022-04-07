d67901a935342e7266f4d63c68e8cc10363ca72e
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-07 15:01:29 +0900

    Time: 12 ms, Memory: 5.8 MB - LeetHub

---
+++ roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        for (int i=0; i<(int)s.size(); i++) {
+            if (i<(int)s.size()-1) {
+                if (s[i] == 'I') {
+                    if (s[i+1] == 'V') {
+                        ans += 4;
+                        i++;
+                        continue;
+                    }
+                    else if (s[i+1] == 'X') {
+                        ans += 9;
+                        i++;
+                        continue;
+                    }
+                }
+                else if (s[i] == 'X') {
+                    if (s[i+1] == 'L') {
+                        ans += 40;
+                        i++;
+                        continue;
+                    }
+                    else if (s[i+1] == 'C') {
+                        ans += 90;
+                        i++;
+                        continue;
+                    }
+                }
+                else if (s[i] == 'C') {
+                    if (s[i+1] == 'D') {
+                        ans += 400;
+                        i++;
+                        continue;
+                    }
+                    else if (s[i+1] == 'M') {
+                        ans += 900;
+                        i++;
+                        continue;
+                    }
+                }
+            }
+            if (s[i] == 'I') ans += 1;
+            else if (s[i] == 'V') ans += 5;
+            else if (s[i] == 'X') ans += 10;
+            else if (s[i] == 'L') ans += 50;
+            else if (s[i] == 'C') ans += 100;
+            else if (s[i] == 'D') ans += 500;
+            else if (s[i] == 'M') ans += 1000;
+        }
+        return ans;
+    }
+};
