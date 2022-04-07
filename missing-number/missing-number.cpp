9ba4b5a75de04d6c894869d3878580ad8f408c42
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-07 16:38:55 +0900

    Time: 28 ms, Memory: 17.9 MB - LeetHub

--- missing-number/missing-number.cpp
+++ missing-number/missing-number.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        vector<bool> chk((int)nums.size()+1);
-        for (int i=0; i<(int)nums.size(); i++) {
-            chk[nums[i]] = 1;
-        }
-        for (int i=0; i<=(int)nums.size(); i++) {
-            if (chk[i] == 0) return i;
-        }
-        return -1;
+        long long n = nums.size();
+        return n*(n+1)/2 - accumulate(nums.begin(), nums.end(), 0LL);
     }
 };
