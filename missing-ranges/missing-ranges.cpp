f0e2a15b38e440a30a7f1921e1b4a460ca780751
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-08 21:09:54 +0900

    Time: 0 ms, Memory: 7.1 MB - LeetHub

--- missing-ranges/missing-ranges.cpp
+++ missing-ranges/missing-ranges.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    vector<string> ans;
-    
-    void conv(int l, int r) {
-        if (r < l) return;
-        else if (l == r) ans.push_back(to_string(l));
-        else ans.push_back(to_string(l) + "->" + to_string(r));
+    string conv(int l, int r) {
+        if (l == r) return to_string(l);
+        return to_string(l) + "->" + to_string(r);
     }
     
     vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
         int n = nums.size();
-        int idx = lower_bound(nums.begin(), nums.end(), lower) - nums.begin();
-        if (idx >= n) {
-            conv(lower, upper);
-            return ans;
-        }
-        if (idx < n && nums[idx] > lower) {
-            conv(lower, nums[idx]-1);
-        }
-        while (++idx < n) {
-            if (nums[idx] > upper) break;
-            conv(nums[idx-1]+1, nums[idx]-1);
-        }
-        if (idx >= n && nums[idx-1] < upper) {
-            conv(nums[idx-1]+1, upper);
+        vector<string> ans;
+        int prev = lower-1;
+        for (int i=0; i<=n; i++) {
+            int cur = (i < n) ? nums[i] : upper + 1;
+            if (prev+1 <= cur-1) {
+                ans.push_back(conv(prev+1, cur-1));
+            }
+            prev = cur;
         }
         return ans;
     }
