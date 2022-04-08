55385284681246deb95ca7fb60c31b913c034fd2
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-08 11:07:22 +0900

    Time: 56 ms, Memory: 20.1 MB - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -5,19 +5,16 @@ public:
         vector<vector<int>> ans;
         for (int i=0; i<(int)nums.size()-2; i++) {
             if (i>0 && nums[i] == nums[i-1]) continue;
-            int f = -nums[i];
             int j = i+1;
             int k = (int)nums.size()-1;
             while (j < k) {
-                int sum = nums[j] + nums[k];
-                if (sum == f) {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    k--;
-                    j++;
-                    while (j < (int)nums.size() && nums[j] == nums[j-1]) j++;
-                    while (k > i && nums[k] == nums[k+1]) k--;
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[j++], nums[k--]});
+                    while (j < k && nums[j] == nums[j-1]) j++;
+                    while (j < k && nums[k] == nums[k+1]) k--;
                 }
-                else if (sum > f) {
+                else if (sum > 0) {
                     k--;
                 }
                 else {
