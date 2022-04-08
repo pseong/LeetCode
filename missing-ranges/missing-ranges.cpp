34dae5d2c7a728a99dc4fb5efe608f4e2469ba98
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-08 20:33:20 +0900

    Time: 6 ms, Memory: 7.1 MB - LeetHub

---
+++ missing-ranges/missing-ranges.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> ans;
+    
+    void conv(int l, int r) {
+        if (r < l) return;
+        else if (l == r) ans.push_back(to_string(l));
+        else ans.push_back(to_string(l) + "->" + to_string(r));
+    }
+    
+    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        int idx = lower_bound(nums.begin(), nums.end(), lower) - nums.begin();
+        if (idx >= n) {
+            conv(lower, upper);
+            return ans;
+        }
+        if (idx < n && nums[idx] > lower) {
+            conv(lower, nums[idx]-1);
+        }
+        while (++idx < n) {
+            if (nums[idx] > upper) {
+                conv(nums[idx]+1, upper);
+                break;
+            }
+            conv(nums[idx-1]+1, nums[idx]-1);
+        }
+        if (idx >= n && nums[idx-1] < upper) {
+            conv(nums[idx-1]+1, upper);
+        }
+        return ans;
+    }
+};
