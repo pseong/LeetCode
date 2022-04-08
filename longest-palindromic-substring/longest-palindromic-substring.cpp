b3b1de1be85138b9f2c02362ced22610031ce911
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-08 16:00:46 +0900

    Time: 638 ms, Memory: 24.6 MB - LeetHub

---
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool chk(int l, int r, string& str) {
+        int s = l;
+        int e = r;
+        while (s < e) {
+            if (str[s] == str[e]) {
+                s++;
+                e--;
+            }
+            else return false;
+        }
+        return true;
+    }
+    
+    string longestPalindrome(string s) {
+        int n = s.size();
+        string ans = "";
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<=i; j++) {
+                if (chk(j, i, s) && ans.size() < i-j+1) {
+                    ans = s.substr(j, i-j+1);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
