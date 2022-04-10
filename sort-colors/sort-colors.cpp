7511e2c8014522bff54bdc9211d8ed115606c738
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-10 19:43:45 +0900

    Time: 0 ms, Memory: 8.4 MB - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int p1 = 0;
+        int p2 = nums.size()-1;
+        for (int i=0; i<=p2; i++) {
+            if (nums[i] == 0) {
+                swap(nums[p1], nums[i]);
+                p1++;
+            }
+            else if (nums[i] == 2) {
+                swap(nums[p2], nums[i]);
+                p2--;
+                i--;
+            }
+        }
     }
 };
