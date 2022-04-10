20e087d6094e2eaefe05bda18106652882d1f382
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-10 18:54:23 +0900

    Time: 0 ms, Memory: 7.9 MB - LeetHub

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    unsigned int chk = 0;
     vector<vector<int>> ans;
     vector<int> nums;
     int n;
     
-    void go(int idx, vector<int>& v) {
+    void go(int idx, vector<int>& nums) {
         if (idx == n) {
-            ans.push_back(v);
+            ans.push_back(nums);
             return;
         }
-        for (int i=0; i<n; i++) {
-            if (chk & 1<<i) continue;
-            chk |= 1<<i;
-            v.push_back(nums[i]);
-            go(idx+1, v);
-            chk &= ~(1<<i);
-            v.pop_back();
+        for (int i=idx; i<n; i++) {
+            swap(nums[idx], nums[i]);
+            go(idx+1, nums);
+            swap(nums[idx], nums[i]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         this->nums = nums;
         this->n = nums.size();
-        vector<int> v;
-        go(0, v);
+        go(0, nums);
         return ans;
     }
 };
