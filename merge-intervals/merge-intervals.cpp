3a213376b2d48e22872fd992f9513fee0ce50572
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-11 14:23:03 +0900

    Time: 60 ms, Memory: 19.1 MB - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
         sort(intervals.begin(), intervals.end());
-        
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
-        for (int i=0; i<n; i++) {
+        for (int i=0; i<intervals.size(); i++) {
             if (ans.back()[1] >= intervals[i][0]) {
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
-                continue;
             }
-            ans.push_back({intervals[i][0], intervals[i][1]});
+            else {
+                ans.push_back({intervals[i][0], intervals[i][1]});
+            }
         }
         return ans;
     }
