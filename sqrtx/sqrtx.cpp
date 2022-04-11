d7e6b38a6f3323950702bdb3b81be5ce669e7318
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-11 21:33:54 +0900

    Time: 0 ms, Memory: 6 MB - LeetHub

--- sqrtx/sqrtx.cpp
+++ sqrtx/sqrtx.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long ans = 1;
-        while (true) {
-            if (ans*ans > x) return ans-1;
-            ans++;
+        int lo = 0;
+        int hi = x;
+        int ans = 0;
+        while (lo <= hi) {
+            long long mid = lo + (hi-lo)/2;
+            if (mid*mid <= x) {
+                ans = mid;
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
         }
+        return ans;
     }
 };
