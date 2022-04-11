6bdb292f861525eec92ba0c596f28c7e8be8d26a
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-11 20:47:49 +0900

    Time: 3 ms, Memory: 5.9 MB - LeetHub

--- factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int five = 0;
-        int two = 0;
-        for (int i=1; i<=n; i++) {
-            int go = i;
-            while (go%2 == 0) {
-                go /= 2;
-                two++;
-            }
-            while (go%5 == 0) {
-                go /= 5;
-                five++;
-            }
+        int ans = 0;
+        while (n > 0) {
+            n /= 5;
+            ans += n;
         }
-        return min(two, five);
+        return ans;
     }
 };
