65370265e359541aa342e51a30fb94948735d46b
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-11 17:59:26 +0900

    Time: 29 ms, Memory: 19.4 MB - LeetHub

--- flatten-2d-vector/flatten-2d-vector.cpp
+++ flatten-2d-vector/flatten-2d-vector.cpp
@@ -1,31 +1,31 @@
 class Vector2D {
 public:
-    vector<vector<int>> v;
+    vector<vector<int>>* v;
     int i, j;
     Vector2D(vector<vector<int>>& vec) {
-        this->v = vec;
+        v = &vec;
         i = 0;
         j = -1;
     }
     
     int next() {
         j++;
-        while (j >= v[i].size()) {
+        while (j >= (*v)[i].size()) {
             i++;
             j = 0;
         }
-        return v[i][j];
+        return (*v)[i][j];
     }
     
     bool hasNext() {
         int a = i;
         int b = j;
         b++;
-        while (a < v.size() && b >= v[a].size()) {
+        while (a < (*v).size() && b >= (*v)[a].size()) {
             a++;
             b = 0;
         }
-        if (a >= v.size() || b >= v[a].size()) return false;
+        if (a >= (*v).size() || b >= (*v)[a].size()) return false;
         return true;
     }
 };
