4bcccb24f0d4333f8b84c5fc19dbdfed9dc26033
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-11 13:40:43 +0900

    Time: 14 ms, Memory: 13.7 MB - LeetHub

--- search-for-a-range/search-for-a-range.cpp
+++ search-for-a-range/search-for-a-range.cpp
@@ -1,9 +1,37 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lo = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        int hi = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
-        if (hi < lo) return {-1, -1};
-        return {lo, hi};
+        if (nums.empty()) return {-1, -1};
+        int n = nums.size();
+        int l = 0;
+        int r = n-1;
+        int lo = -1;
+        while (l <= r) {
+            int mid = l+(r-l)/2;
+            if (nums[mid] >= target) {
+                lo = mid;
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        if (lo==-1 || lo==n || nums[lo] != target) return {-1, -1};
+        
+        l = 0;
+        r = n-1;
+        int hi = -1;
+        while (l <= r) {
+            int mid = l+(r-l)/2;
+            if (nums[mid] > target) {
+                hi = mid;
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        if (hi == -1) hi = n;
+        return {lo, hi-1};
     }
 };
