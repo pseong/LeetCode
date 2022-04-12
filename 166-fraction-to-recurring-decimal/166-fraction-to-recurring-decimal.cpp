0d2be0485c1cdb8b557bd4e90a968fddc7b72f2f
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-12 12:22:41 +0900

    Time: 3 ms (40.60%), Space: 6.4 MB (9.48%) - LeetHub

---
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) return "0";
+        long long num = numerator;
+        long long den = denominator;
+        
+        int cnt = 0;
+        if (num < 0) {
+            cnt++;
+            num = -num;
+        }
+        if (den < 0) {
+            cnt++;
+            den = -den;
+        }
+        string sign;
+        if (cnt == 1) sign = "-";
+        
+        vector<string> fraction;
+        unordered_map<int, int> chk;
+        
+        long long a = num / den;
+        num -= a*den;
+        
+        while (num != 0) {
+            chk[num] = fraction.size();
+            num *= 10;
+            long long b = num / den;
+            fraction.push_back(to_string(b));
+            if (b>0) {
+                num -= b*den;
+                if (chk[num]) {
+                    string ans = sign + to_string(a) + ".";
+                    for (int i=0; i<chk[num]; i++) {
+                        ans += fraction[i];
+                    }
+                    ans += "(";
+                    for (int i=chk[num]; i<fraction.size(); i++) {
+                        ans += fraction[i];
+                    }
+                    ans += ")";
+                    return ans;
+                }
+            }
+        }
+        string ans = sign + to_string(a);
+        if (fraction.size()) {
+            ans += ".";
+            for (int i=0; i<fraction.size(); i++) {
+                ans += fraction[i];
+            }
+        }
+        return ans;
+    }
+};
