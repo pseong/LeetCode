2bf3905ebab19b11178ac8bf66f07a832600dbc9
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-12 14:28:25 +0900

    Time: 5 ms (6.93%), Space: 5.8 MB (95.64%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        do {
+        while (b) {
             pair<int, int> tmp = {a^b, a&b};
             tie(a, b) = tmp;
             b &= ~(1<<31);
             b <<= 1;
-        } while (b);
+        }
         return a;
     }
 };
