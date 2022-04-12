22ea3438c4af8337fcbc4f96353704f19d99bec1
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-12 14:18:22 +0900

    Time: 0 ms, Memory: 6 MB - LeetHub

--- sum-of-two-integers/sum-of-two-integers.cpp
+++ sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-
-        
         do {
             pair<int, int> p = {a&b, a^b};
             tie(b, a) = p;
             b &= ~(1<<31);
             b <<= 1;
         } while (b);
-        
         return a;
     }
 };
