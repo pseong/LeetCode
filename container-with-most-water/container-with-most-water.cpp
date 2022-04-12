202cd8e82b7500aec72d6cb795afd54297020072
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-13 02:32:46 +0900

    Time: 1787 ms, Memory: 83.5 MB - LeetHub

---
+++ container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int n = nums1.size();
+        vector<int> one;
+        vector<int> two;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                one.push_back(nums1[i] + nums2[j]);
+            }
+        }
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                two.push_back(nums3[i] + nums4[j]);
+            }
+        }
+        sort(one.begin(), one.end());
+        sort(two.begin(), two.end());
+        int ans = 0;
+        for (int i=0; i<one.size(); i++) {
+            ans += upper_bound(two.begin(), two.end(), -one[i])
+                - lower_bound(two.begin(), two.end(), -one[i]);
+        }
+        return ans;
+    }
+};
