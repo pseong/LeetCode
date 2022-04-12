b607c879ee2974a93a19e035bd2de904a6c22c46
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-12 10:49:09 +0900

    Time: 0 ms, Memory: 6.1 MB - LeetHub

--- divide-two-integers/divide-two-integers.cpp
+++ divide-two-integers/divide-two-integers.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int HALF_INT_MIN = -1073741824;
+    vector<int> divs;
+    vector<int> pows;
     
     int divide(int dividend, int divisor) {
         if (dividend == INT_MIN && divisor == -1) return INT_MAX;
@@ -15,16 +17,22 @@ public:
             divisor = -divisor;
         }
         
+        int val = divisor;
+        int e = -1;
+        while (val >= dividend) {
+            divs.push_back(val);
+            pows.push_back(e);
+            if (val < HALF_INT_MIN) break;
+            val += val;
+            e += e;
+        }
+        
         int ans = 0;
-        while (divisor >= dividend) {
-            int exp = -1;
-            int val = divisor;
-            while (val >= HALF_INT_MIN && val + val >= dividend) {
-                val += val;
-                exp += exp;
+        for (int i=divs.size()-1; i>=0; i--) {
+            if (dividend <= divs[i]) {
+                dividend -= divs[i];
+                ans += pows[i];
             }
-            ans += exp;
-            dividend -= val;
         }
         
         if (neg != 1) ans = -ans;
