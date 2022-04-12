e2191a3e96670a98eb195733975fb7965e567e7c
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-12 10:24:54 +0900

    Time: 0 ms, Memory: 6 MB - LeetHub

--- divide-two-integers/divide-two-integers.cpp
+++ divide-two-integers/divide-two-integers.cpp
@@ -1,9 +1,33 @@
 class Solution {
 public:
+    int HALF_INT_MIN = -1073741824;
+    
     int divide(int dividend, int divisor) {
         if (dividend == INT_MIN && divisor == -1) return INT_MAX;
-        if (divisor == 1) return dividend;
-        else if (divisor == -1) return -dividend;
-        return (long long)dividend / divisor;
+        
+        int neg = 2;
+        if (dividend > 0) {
+            neg--;
+            dividend = -dividend;
+        }
+        if (divisor > 0) {
+            neg--;
+            divisor = -divisor;
+        }
+        
+        int ans = 0;
+        while (divisor >= dividend) {
+            int exp = -1;
+            int val = divisor;
+            while (val >= HALF_INT_MIN && val + val >= dividend) {
+                val += val;
+                exp += exp;
+            }
+            ans += exp;
+            dividend -= val;
+        }
+        
+        if (neg != 1) ans = -ans;
+        return ans;
     }
 };
