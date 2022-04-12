dbb653e0720d0475f0275f9f949e1f7dd8536304
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-13 02:36:40 +0900

    Time: 406 ms, Memory: 24.4 MB - LeetHub

--- container-with-most-water/container-with-most-water.cpp
+++ container-with-most-water/container-with-most-water.cpp
@@ -2,29 +2,19 @@ class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         int n = nums1.size();
-        
-        vector<int> one;
-        vector<int> two;
-        
+        unordered_map<int, int> m;
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                one.push_back(nums1[i] + nums2[j]);
+                m[nums1[i]+nums2[j]]++;
             }
         }
-        
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                two.push_back(nums3[i] + nums4[j]);
-            }
-        }
-        
-        sort(one.begin(), one.end());
-        sort(two.begin(), two.end());
-        
         int ans = 0;
-        for (int i=0; i<one.size(); i++) {
-            ans += upper_bound(two.begin(), two.end(), -one[i])
-                - lower_bound(two.begin(), two.end(), -one[i]);
+        for (int a : nums3) {
+            for (int b : nums4) {
+                if (m.find(-(a+b)) != m.end()) {
+                    ans += m[-(a+b)];
+                }
+            }
         }
         return ans;
     }
