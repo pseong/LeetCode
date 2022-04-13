597357ff3b96e0e179bd88334fe3aa07f8714494
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-13 19:21:01 +0900

    Time: 108 ms, Memory: 61.3 MB - LeetHub

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        while (nums[0] != nums[nums[0]])
-            swap(nums[0], nums[nums[0]]);
-        return nums[0];
+        int slow = 0;
+        int fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[fast];
+            fast = nums[fast];
+        } while (slow != fast);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
     }
 };
