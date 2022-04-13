e4c889f84010fea7fa2a12e878a995b3e860be26
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-13 16:26:01 +0900

    Time: 201 ms (63.73%), Space: 82.9 MB (96.63%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         nums.push_back(0);
         for (int i=0; i<n;) {
-            if (nums[i]>=0 && nums[i]<=n) {
+            if (nums[i]>=1 && nums[i]<=n) {
                 if (nums[i] == nums[nums[i]]) i++;
                 else swap(nums[i], nums[nums[i]]);
             }
