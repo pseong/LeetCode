7aa1239c6986d93ece90ccbff3c042c17d3ce470
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-13 14:51:26 +0900

    Time: 4 ms, Memory: 6.8 MB - LeetHub

--- game-of-life/game-of-life.cpp
+++ game-of-life/game-of-life.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> b;
-    int n, m;
-    
-    int cal(int x, int y) {
+    int cal(int x, int y, int n, int m, vector<vector<int>>& board) {
         int cnt = 0;
         for (int i=x-1; i<=x+1; i++) {
             for (int j=y-1; j<=y+1; j++) {
                 if (i<0 || i>=n || j<0 || j>=m || (x==i&&y==j)) continue;
-                if (b[i][j]) cnt++;
+                if (board[i][j]) cnt++;
             }
         }
         return cnt;
     }
     
     void gameOfLife(vector<vector<int>>& board) {
-        b = board;
-        n = board.size();
-        m = board[0].size();
+        int n = board.size();
+        int m = board[0].size();
         
         vector<vector<int>> ans(n, vector<int>(m, 0));
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
-                int cnt = cal(i, j);
+                int cnt = cal(i, j, n, m, board);
                 if (board[i][j]) {
                     if (cnt == 2 || cnt == 3) ans[i][j] = 1;
                 }
