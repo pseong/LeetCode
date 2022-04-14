6e979a3557a44d80943eff93c8706dfdc254f7a0
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-14 15:41:23 +0900

    Time: 27 ms, Memory: 22.8 MB - LeetHub

--- merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,16 +11,29 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* before = new ListNode(-100000);
-        for (ListNode* ln : lists) {
-            merge(before, ln);
-        }
-        return before->next;
+        if (lists.empty()) return nullptr;
+        return mergeList(lists, 0, lists.size()-1);
     }
     
-    void merge(ListNode* a, ListNode* b) {
-        ListNode* go = a;
-        a = a->next;
+    ListNode* mergeList(vector<ListNode*>& lists, int l, int r) {
+        if (l == r) return lists[l];
+        ListNode* a;
+        ListNode* b;
+        if (r-l > 1) {
+            int mid = (l+r)/2;
+            a = mergeList(lists, l, mid);
+            b = mergeList(lists, mid+1, r);
+        }
+        else {
+            a = lists[l];
+            b = lists[r];
+        }
+        return merge(a, b);
+    }
+    
+    ListNode* merge(ListNode* a, ListNode* b) {
+        ListNode* go = new ListNode(-100000, a);
+        ListNode* ans = go;
         while (a && b) {
             if (a->val <= b->val) {
                 go->next = a;
@@ -34,5 +47,6 @@ public:
         }
         if (b) go->next = b;
         else if (a) go->next = a;
+        return ans->next;
     }
 };
