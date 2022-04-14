48e73c0019a043068eeee5173f703ca9474c00f6
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-15 00:16:47 +0900

    Time: 11 ms, Memory: 10.2 MB - LeetHub

--- surrounded-regions/surrounded-regions.cpp
+++ surrounded-regions/surrounded-regions.cpp
@@ -5,10 +5,10 @@ public:
     int dx[4]{-1, 0, 0, 1};
     int dy[4]{0, -1, 1, 0};
     
-    void bfs(int i, int j) {
+    void bfs(vector<vector<char>>& b, int i, int j) {
         queue<pair<int, int>> q;
         q.push({i, j});
-        b[i][j] = 'X';
+        b[i][j] = '#';
         while (q.size()) {
             tie(i, j) = q.front(); q.pop();
             for (int d=0; d<4; d++) {
@@ -16,7 +16,7 @@ public:
                 int y = j+dy[d];
                 if (x<0 || x>=n || y<0 || y>=m) continue;
                 if (b[x][y] == 'O') {
-                    b[x][y] = 'X';
+                    b[x][y] = '#';
                     q.push({x, y});
                 }
             }
@@ -24,29 +24,32 @@ public:
     }
     
     void solve(vector<vector<char>>& board) {
-        b = board;
+        vector<vector<char>>& b = board;
         n = b.size();
         m = b[0].size();
         for (int j=0; j<m; j++) {
             if (b[0][j] == 'O') {
-                bfs(0, j);
+                bfs(b, 0, j);
             }
             if (b[n-1][j] == 'O') {
-                bfs(n-1, j);
+                bfs(b, n-1, j);
             }
         }
         for (int i=0; i<n; i++) {
             if (b[i][0] == 'O') {
-                bfs(i, 0);
+                bfs(b, i, 0);
             }
             if (b[i][m-1] == 'O') {
-                bfs(i, m-1);
+                bfs(b, i, m-1);
             }
         }
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
                 if (b[i][j] == 'O') {
                     board[i][j] = 'X';
+                }
+                else if (b[i][j] == '#') {
+                    board[i][j] = 'O';
                 }
             }
         }
