b1a2e7f2d19c4efb1d6942b57c7353b44101b7a0
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-15 02:40:26 +0900

    Time: 24 ms (83.89%), Space: 14.2 MB (29.47%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> chk(n, 0);
+        int cnt = 0;
+        for (int i=0; i<n; i++) {
+            if (!chk[i]) {
+                cnt++;
+                chk[i] = 1;
+                queue<int> q;
+                q.push(i);
+                while (q.size()) {
+                    int x = q.front(); q.pop();
+                    for (int y=0; y<n; y++) {
+                        if (!chk[y] && isConnected[x][y]) {
+                            chk[y] = 1;
+                            q.push(y);
+                        }
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+};
