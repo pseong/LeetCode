0d1589e063d425f69ea46c20ba984a7831fd1d4a
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-14 23:41:32 +0900

    Time: 107 ms (86.22%), Space: 14.3 MB (51.12%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     vector<int> adj[5000];
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        wordList.push_back(beginWord);
         unordered_map<string, int> m;
         for (int i=0; i<wordList.size(); i++) {
             m[wordList[i]] = i;
         }     
+        if (m.find(endWord) == m.end()) return 0;
         int ans[5000]{ 0 };
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1});
+        queue<int> q;
+        ans[(int)wordList.size()-1] = 1;
+        q.push((int)wordList.size()-1);
         while (q.size()) {
-            int cnt;
-            string s;
-            tie(s, cnt) = q.front(); q.pop();
+            int idx = q.front(); q.pop();
+            string& s = wordList[idx];
             for (int i=0; i<s.size(); i++) {
                 char tmp = s[i];
                 for (char c='a'; c<='z'; c++) {
                     s[i] = c;
                     auto it = m.find(s);
                     if (it != m.end() && !ans[it->second]) {
-                        if (s == endWord) {
-                            return cnt+1;
+                        if (it->second == m[endWord]) {
+                            return ans[idx] + 1;
                         }
-                        ans[it->second] = cnt + 1;
-                        q.push({it->first, cnt+1});
+                        ans[it->second] = ans[idx] + 1;
+                        q.push(it->second);
                     }
                 }
                 s[i] = tmp;
