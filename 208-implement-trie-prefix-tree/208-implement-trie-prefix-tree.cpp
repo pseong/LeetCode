21b60d8a9c356051f17c0fab55e0280625147f1f
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-16 03:32:40 +0900

    Time: 48 ms (98.14%), Space: 45.1 MB (48.07%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,65 @@
+class Trie {
+public:
+    struct Node {
+        Node* child[26] { 0 };
+        char c { 0 };
+        bool end { 0 };
+        
+        Node() {}
+        Node(char c) {
+            this->c = c;
+        }
+    };
+    Node* root;
+    
+    void _insert(Node* node, string& word, int idx) {
+        if (idx == word.size()) {
+            node->end = 1;
+            return;
+        }
+        if (node->child[word[idx]-'a']) {
+            _insert(node->child[word[idx]-'a'], word, idx+1);
+            return;
+        }
+        Node* n = new Node(word[idx]);
+        node->child[word[idx]-'a'] = n;
+        _insert(n, word, idx+1);
+    }
+    
+    bool _search(Node* node, string& word, int idx) {
+        if (idx == word.size()) return (node->end);
+        if (!node->child[word[idx]-'a']) return false;
+        return _search(node->child[word[idx]-'a'], word, idx+1);
+    }
+    
+    bool _startsWith(Node* node, string& word, int idx) {
+        if (idx == word.size()) return true;
+        if (!node->child[word[idx]-'a']) return false;
+        return _startsWith(node->child[word[idx]-'a'], word, idx+1);
+    }
+    
+    Trie() {
+        root = new Node(0);
+        root->end = 1;
+    }
+    
+    void insert(string word) {
+        _insert(root, word, 0);
+    }
+    
+    bool search(string word) {
+        return _search(root, word, 0);
+    }
+    
+    bool startsWith(string prefix) {
+        return _startsWith(root, prefix, 0);
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
