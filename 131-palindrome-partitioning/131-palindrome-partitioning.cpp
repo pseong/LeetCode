ca4003e9f95917929d1d60d7cc79e19aa888cf00
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-16 00:59:36 +0900

    Time: 145 ms (67.97%), Space: 75.7 MB (43.10%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,24 +1,42 @@
 class Solution {
-	vector<vector<string> > partitionRes;
-	vector<string> oneRes;
 public:
-    	void findpartition(const string &s){
-    		for(int i=1; i<=s.size(); i++){
-    		    int l=0, r=i-1;
-    			while( l <= r) {
-    			    if(s[l] != s[r]) break;
-    			    l++; r--;
-    			}
-    			if(l > r){
-    				oneRes.push_back(s.substr(0 ,i));
-    				if(i == s.size())partitionRes.push_back(oneRes);
-    				findpartition(s.substr(i));
-    				oneRes.pop_back();
-    			}
-    		}
-    	}
-    	vector<vector<string> > partition(string s) {
-	    	findpartition(s);
-		    return partitionRes;
-    	}
+    vector<vector<string>> ans;
+    vector<string> now;
+    void go(int idx, string& s, vector<vector<bool>>& pal) {
+        if (idx == s.size()) {
+            ans.push_back(now);
+            return;
+        }
+        for (int i=idx; i<s.size(); i++) {
+            if (pal[idx][i]) {
+                now.push_back(s.substr(idx, i-idx+1));
+                go(i+1, s, pal);
+                now.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<string>> partition(string s) {
+        vector<vector<bool>> pal(s.size(), vector<bool>(s.size(), 0));
+        for (int i=0; i<s.size(); i++) {
+            for (int k=0; k<s.size(); k++) {
+                int a = i-k;
+                int b = i+k;
+                if (a<0 || b >=s.size()) break;
+                if (s[a] == s[b]) pal[a][b] = 1;
+                else break;
+            }
+        }
+        for (int i=0; i<s.size(); i++) {
+            for (int k=0; k<s.size(); k++) {
+                int a = i-k;
+                int b = i+1+k;
+                if (a<0 || b >=s.size()) break;
+                if (s[a] == s[b]) pal[a][b] = 1;
+                else break;
+            }
+        }
+        go(0, s, pal);
+        return ans;
+    }
 };
