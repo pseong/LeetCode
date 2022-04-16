7fd2216751693be6aa447973023fae4bda7253bf
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-16 12:01:42 +0900

    Time: 63 ms (83.82%), Space: 45 MB (48.20%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,59 +1,44 @@
 class Trie {
 public:
     struct Node {
-        Node* child[26] { 0 };
-        char c { 0 };
-        bool end { 0 };
-        
-        Node() {}
-        Node(char c) {
-            this->c = c;
-        }
+        Node* children[26];
+        bool end;
     };
-    Node* root;
     
-    void _insert(Node* node, string& word, int idx) {
-        if (idx == word.size()) {
-            node->end = 1;
-            return;
-        }
-        Node* child = node->child[word[idx]-'a'];
-        if (child) {
-            _insert(child, word, idx+1);
-            return;
-        }
-        child = new Node(word[idx]);
-        node->child[word[idx]-'a'] = child;
-        _insert(child, word, idx+1);
-    }
-    
-    bool _search(Node* node, string& word, int idx) {
-        if (idx == word.size()) return (node->end);
-        if (!node->child[word[idx]-'a']) return false;
-        return _search(node->child[word[idx]-'a'], word, idx+1);
-    }
-    
-    bool _startsWith(Node* node, string& word, int idx) {
-        if (idx == word.size()) return true;
-        if (!node->child[word[idx]-'a']) return false;
-        return _startsWith(node->child[word[idx]-'a'], word, idx+1);
-    }
+    Node* root = nullptr;
     
     Trie() {
-        root = new Node(0);
-        root->end = 1;
+        root = new Node();
     }
     
     void insert(string word) {
-        _insert(root, word, 0);
+        Node* cur = root;
+        for (int i=0; i<word.size(); i++) {
+            if (!cur->children[word[i]-'a']) {
+                Node* n = new Node();
+                cur->children[word[i]-'a'] = n;
+            }
+            cur = cur->children[word[i]-'a'];
+        }
+        cur->end = true;
     }
     
     bool search(string word) {
-        return _search(root, word, 0);
+        Node* cur = root;
+        for (int i=0; i<word.size(); i++) {
+            if (!cur->children[word[i]-'a']) return false;
+            cur = cur->children[word[i]-'a'];
+        }
+        return cur->end;
     }
     
     bool startsWith(string prefix) {
-        return _startsWith(root, prefix, 0);
+        Node* cur = root;
+        for (int i=0; i<prefix.size(); i++) {
+            if (!cur->children[prefix[i]-'a']) return false;
+            cur = cur->children[prefix[i]-'a'];
+        }
+        return true;
     }
 };
 
