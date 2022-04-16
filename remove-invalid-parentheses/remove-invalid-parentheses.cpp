b6ecf1812c5a5438d1c7bd2f54f0de16175bb07f
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-16 18:16:45 +0900

    Time: 148 ms, Memory: 8.8 MB - LeetHub

---
+++ remove-invalid-parentheses/remove-invalid-parentheses.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string s;
+    unordered_set<string> ans;
+    string now;
+    int remove;
+    void go(int idx, int open, int close) {
+        if (idx == s.size()) {
+            if (open == close && s.size()-now.size() == remove) {
+                ans.insert(now);
+            }
+            return;
+        }
+        if (s[idx] >= 'a' && s[idx] <= 'z') {
+            now.push_back(s[idx]);
+            go(idx+1, open, close);
+            now.pop_back();
+            return;
+        }
+        go(idx+1, open, close);
+        now.push_back(s[idx]);
+        if (s[idx] == '(') {
+            go(idx+1, open+1, close);
+        }
+        else if (s[idx] == ')') {
+            if (open >= close+1) go(idx+1, open, close+1);
+        }
+        now.pop_back();
+    }
+    
+    vector<string> removeInvalidParentheses(string s) {
+        this->s = s;
+        int open = 0;
+        int close = 0;
+        for (int i=0; i<s.size(); i++) {
+            if (s[i] == '(') open++;
+            else if (s[i] == ')') {
+                close++;
+                if (close > open) {
+                    remove++;
+                    close--;
+                }
+            }
+        }
+        remove += abs(open - close);
+        go(0, 0, 0);
+        return vector<string>(ans.begin(), ans.end());
+    }
+};
