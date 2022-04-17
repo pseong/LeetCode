6be7f5ee3474dcbfc5239110ad781dac2a088669
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-17 18:55:44 +0900

    Time: 24 ms, Memory: 17.3 MB - LeetHub

--- wiggle-sort-ii/wiggle-sort-ii.cpp
+++ wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
+    int n;
+    
+    /*
+    bool f(int i) {
+        return (i*2+1)%(n|1);
+    }*/
+    
     void wiggleSort(vector<int>& nums) {
-        int n = nums.size();
-
-        // Find a median.
-        auto midptr = nums.begin() + n / 2;
-        nth_element(nums.begin(), midptr, nums.end());
-        int mid = *midptr;
-
-        // Index-rewiring.
-        #define A(i) nums[(1+2*(i)) % (n|1)]
-
-        // 3-way-partition-to-wiggly in O(n) time with O(1) space.
-        int i = 0, j = 0, k = n - 1;
+        n = nums.size();
+        
+        nth_element(nums.begin(), nums.begin() + n/2, nums.end());
+        int mid = *(nums.begin()+n/2);
+        #define f(i) nums[(1+2*(i)) % (n|1)]
+        int i, j, k;
+        i = j = 0;
+        k = n-1;
         while (j <= k) {
-            if (A(j) > mid)
-                swap(A(i++), A(j++));
-            else if (A(j) < mid)
-                swap(A(j), A(k--));
-            else
+            if (f(j) > mid) {
+                swap(f(j++), f(i++));
+            }
+            else if (f(j) < mid) {
+                swap(f(j), f(k--));
+            }
+            else {
                 j++;
+            }
         }
     }
 };
