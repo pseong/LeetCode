2648dad207163efbb08816eef97fded213d34a51
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-17 14:45:25 +0900

    Time: 7 ms (64.33%), Space: 7 MB (59.74%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -6,10 +6,11 @@ public:
         dp[0][0] = true;
         for (int i = 0; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i][j - 2] || (i && dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
-                } else {
-                    dp[i][j] = i && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                if (p[j-1] == '*') {
+                    dp[i][j] = dp[i][j-2] || (i && dp[i-1][j] && (p[j-2] == '.' || p[j-2] == s[i-1]));
+                }
+                else {
+                    dp[i][j] = i && dp[i-1][j-1] && (p[j-1] == '.' || p[j-1] == s[i-1]);
                 }
             }
         }
