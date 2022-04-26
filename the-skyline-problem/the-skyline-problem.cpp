136661be69fb876e560e541577caf80fb1c636e4
박성욱 <76799354+pseong@users.noreply.github.com>
2022-04-26 15:59:39 +0900

    Time: 55 ms, Memory: 17.1 MB - LeetHub

--- the-skyline-problem/the-skyline-problem.cpp
+++ the-skyline-problem/the-skyline-problem.cpp
@@ -1,51 +1,26 @@
 class Solution {
 public:
-    
-    vector<vector<int>> merge(vector<vector<int>>& l, vector<vector<int>> r) {
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        vector<vector<int>> v;
+        for (vector<int>& wall : buildings) {
+            v.push_back({wall[0], -wall[2]});
+            v.push_back({wall[1], wall[2]});
+        }
+        sort(v.begin(), v.end());
+        multiset<int> ms;
         vector<vector<int>> ans;
-        int leftY=0, rightY=0, curY=0;
-        int idxL=0, idxR=0;
-        while (!(idxL == l.size() && idxR == r.size())) {
-            if (idxR == r.size() || idxL != l.size() && (l[idxL][0] < r[idxR][0])) {
-                if (l[idxL][1] < curY) {
-                    int pre = curY;
-                    curY = max(rightY, l[idxL][1]);
-                    if (pre != curY) ans.push_back({l[idxL][0], curY});
-                }
-                else if (l[idxL][1] > curY) {
-                    curY = l[idxL][1];
-                    ans.push_back({l[idxL][0], curY});
-                }
-                leftY = l[idxL][1];
-                idxL++;
-            }
-            else if (idxL == l.size() || idxR != r.size() && (l[idxL][0] > r[idxR][0])) {
-                swap(leftY, rightY);
-                swap(idxL, idxR);
-                swap(l, r);
+        for (vector<int>& wall : v) {
+            if (wall[1] < 0) {
+                ms.insert(-wall[1]);
             }
             else {
-                int pre = curY;
-                curY = max(l[idxL][1], r[idxR][1]);
-                if (pre != curY) ans.push_back({l[idxL][0], curY});
-                leftY = l[idxL][1];
-                rightY = r[idxR][1];
-                idxL++;
-                idxR++;
+                ms.erase(ms.find(wall[1]));
             }
+            if (ms.size() && (!ans.size() || ans.size() && ans.back()[1] != *ms.rbegin())) {
+                ans.push_back({wall[0], *ms.rbegin()});
+            }
+            else if (!ms.size()) ans.push_back({wall[0], 0});
         }
         return ans;
-    }
-    
-    vector<vector<int>> divide(vector<vector<int>>& b, int l, int r) {
-        if (l==r) return {{b[l][0], b[l][2]}, {b[l][1], 0}};
-        int mid = (l+r)/2;
-        vector<vector<int>> left = divide(b, l, mid);
-        vector<vector<int>> right = divide(b, mid+1, r);
-        return merge(left, right);
-    }
-    
-    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
-        return divide(buildings, 0, buildings.size()-1);
     }
 };
